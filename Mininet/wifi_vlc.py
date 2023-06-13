33c9d53cdb3d85d38a696a455ff4b0c6cfe8ab3c
ErickSantosRodrigues <esrerickssantos@gmail.com>
2023-06-13 10:43:26 -0300

    a

--- Mininet/wifi_vlc.py
+++ Mininet/wifi_vlc.py
@@ -1,7 +1,7 @@
 from mininet.node import Controller
 from mininet.log import setLogLevel, info
 from mn_wifi.node import OVSKernelAP
-from mn_wifi.cli import CLI_wifi
+from mn_wifi.cli import CLI
 from mn_wifi.net import Mininet_wifi
 from mininet.topo import Topo
 
