a4ea7f2ada8a507de1446e705c69d56389868619
ErickSantosRodrigues <esrerickssantos@gmail.com>
2023-07-04 18:35:44 -0300

    32

--- Ryu_tests/test.py
+++ Ryu_tests/test.py
@@ -2,7 +2,6 @@ from ryu.base import app_manager
 from ryu.controller import ofp_event
 from ryu.controller.handler import MAIN_DISPATCHER, set_ev_cls
 from ryu.ofproto import ofproto_v1_3 as odp
-from ryu.lib.packet import packet, ethernet, ether_types, ipv4
 
 class MyController(app_manager.RyuApp):
     """Ryu App to manage open flow rules"""
@@ -11,8 +10,8 @@ class MyController(app_manager.RyuApp):
     def __init__(self, *args, **kwargs):
         super(MyController, self).__init__(*args, **kwargs)
         self.client1_streaming = False
-        self.client1_mac = "00:00:00:00:00:02" # Substitute with actual MAC
-        self.client2_mac = "00:00:00:00:00:03" # Substitute with actual MAC
+        self.client1_mac = "00:00:00:00:00:01" # Substitute with actual MAC
+        self.client2_mac = "00:00:00:00:00:02" # Substitute with actual MAC
 
     @set_ev_cls(ofp_event.EventOFPPacketIn, MAIN_DISPATCHER)
     def packet_in_handler(self, event):
@@ -29,13 +28,13 @@ class MyController(app_manager.RyuApp):
                 if eth.src == self.client2_mac:
                     self.add_flow(event, priority=1, actions=[])
                 else:
-                    actions = [event.msg.datapath.ofproto_parser.OFPActionOutput(odp.OFPP_FLOOD)]
+                    actions = [parser.OFPActionOutput(ofp.OFPP_FLOOD)]
                     self.add_flow(event, actions=actions)
             else:
                 # Check if client 1 is streaming
                 if eth.src == self.client1_mac:
                     self.client1_streaming = True
-                actions = [event.msg.datapath.ofproto_parser.OFPActionOutput(odp.OFPP_FLOOD)]
+                actions = [parser.OFPActionOutput(ofp.OFPP_FLOOD)]
                 self.add_flow(event, actions=actions)
 
     def add_flow(self, event, priority, actions, buffer_id=None):
