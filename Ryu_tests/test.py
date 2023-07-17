2a7d25533d566555ef45638157245bc4016b4f1d
ErickSantosRodrigues <esrerickssantos@gmail.com>
2023-07-17 15:56:17 -0300

    att3

--- Ryu_tests/test.py
+++ Ryu_tests/test.py
@@ -12,35 +12,37 @@ class QueueController(app_manager.RyuApp):
 
     @set_ev_cls(ofp_event.EventOFPStateChange, [MAIN_DISPATCHER])
     def state_change_handler(self, ev):
-        datapath = ev.datapath
-        ofproto = datapath.ofproto
+        datapath = ev.msg.datapath
+        ofproto = datapath.ofproto 
         parser = datapath.ofproto_parser
-        match = parser.OFPMatch(in_port=1)
-        actions = [parser.OFPActionOutput(2)]
-        bucket = ofproto_v1_3_parser.OFPBucket(weight=0, watch_port=ofproto_v1_3.OFPP_ANY,
-                                                watch_group=ofproto_v1_3.OFPG_ANY, actions=actions)
-        req = ofproto_v1_3_parser.OFPQueueGetConfigRequest(ev.datapath, 1)
+        match = parser.OFPMatch()
+        actions = [parser.OFPActionOutput(ofproto.OFPP_CONTROLLER, ofproto.OFPCML_NO_BUFFER)]
+        inst = [parser.OFPInstructionActions(ofproto.OFPIT_APPLY_ACTIONS , actions)]
+        mod = datapath.ofproto_parser.OFPFlowMod(
+                datapath=datapath, match=match, cookie=0,
+                command=ofproto.OFPFC_ADD, idle_timeout=0, hard_timeout=0,
+                priority=0, instructions=inst)
+        datapath.send_msg(mod)
+        port_1 = 3
+        queue_1 = parser.OFPActionSetQueue(0)
+        actions_1 = [queue_1, parser.OFPActionOutput(port_1)]
 
+        watch_port = ofproto_v1_3.OFPP_ANY
+        watch_group = ofproto_v1_3.OFPQ_ALL
+        weight_1 = 1
+        buckets = [parser.OFPBucket(weight_1, watch_port, watch_group, actions_1)]
+        group_id = 50
+        req = parser.OFPGroupMod(
+            datapath, datapath.ofproto.OFPFC_ADD,
+            datapath.ofproto.OFPGT_SELECT, group_id, buckets)
         datapath.send_msg(req)
-
-        ofproto_v1_3_parser.OFPQueueModify(ev.datapath, 1, 1, min_rate=1000)
-
-        # create a queue with id=1, max_rate=1000 kbps
-        prop = parser.OFPQueuePropMaxRate(ofproto.OFPQT_MIN_RATE, 100_000)
-
-        # construct a flow_mod message and send it to the switch
-        instructions = [parser.OFPInstructionActions(ofproto.OFPIT_APPLY_ACTIONS, actions)]
-        flow_mod = parser.OFPFlowMod(
-            datapath=datapath,
-            match=match,
-            cookie=0,
-            command=ofproto.OFPFC_ADD,
-            idle_timeout=0,
-            hard_timeout=0,
-            priority=100,
-            flags=ofproto.OFPFF_SEND_FLOW_REM,
-            actions=actions
-        )
-
-        datapath.send_msg(flow_mod)
-
+        
+        match = parser.OFPMatch(in_port=1, ipv4_src="10.0.0.1", ipv4_dst="10.0.0.2")
+        actions = [datapath.ofproto_parser.OFPActionGroup(50)]
+        inst = [parser.OFPInstructionActions(ofproto.OFPIT_APPLY_ACTIONS, actions)]
+        mod = datapath.ofproto_parser.OFPFlowMod(
+            datapath=datapath, match=match, cookie=0,
+            command=ofproto.OFPFC_ADD, idle_timeout=0, hard_timeout=0,
+            priority=3, instructions=inst)
+        datapath.send_msg(mod)
+        
