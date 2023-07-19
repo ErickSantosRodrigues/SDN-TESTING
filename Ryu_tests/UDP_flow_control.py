d453cbe2a4722d3c22f724673ed4465a3889b264
ErickSantosRodrigues <esrerickssantos@gmail.com>
2023-07-19 14:24:32 -0300

    att

--- Ryu_tests/UDP_flow_control.py
+++ Ryu_tests/UDP_flow_control.py
@@ -154,70 +154,20 @@ class ProjectController(app_manager.RyuApp):
     @set_ev_cls(ofp_event.EventOFPPacketIn, MAIN_DISPATCHER)
     def _packet_in_handler(self, ev):
         msg = ev.msg
-        datapath = msg.datapath
-        ofproto = datapath.ofproto
-        parser = datapath.ofproto_parser
+        dp = msg.datapath
+        dpid = dp.id
+        ofproto = dp.ofproto
+        parser = dp.ofproto_parser
+
+        pkt = packet.Packet(msg.data)
+        eth = pkt.get_protocols(ethernet.ethernet)[0]
+
+        src, dst = eth.src, eth.dst
         in_port = msg.match['in_port']
- # 
- #        pkt = packet.Packet(msg.data)
- #        eth = pkt.get_protocol(ethernet.ethernet)
- # 
- #        dst = eth.dst
- #        src = eth.src
- #        dpid = datapath.id
- #        self.mac_to_port.setdefault(dpid, {})
- #        # print "nodes"
- #        # print self.net.nodes()
- #        # print "edges"
- #        # print self.net.edges()
- #        # self.logger.info("packet in %s %s %s %s", dpid, src, dst, in_port)
- #       
- #        if src not in self.net:
- #            self.net.add_node(src)
- #            # self.net.add_edge(dpid, src, {'port': in_port})
- #            self.net.add_edge(src, dpid)
- #        if dst in self.net:
- #            # print (src in self.net)
- #            # print nx.shortest_path(self.net,1,4)
- #            # print nx.shortest_path(self.net,4,1)
- #            # print nx.shortest_path(self.net,src,4)
- # 
- #            path = nx.shortest_path(self.net, src, dst)  
- #            next = path[path.index(dpid)+1]
- #            out_port = self.net[dpid][next]['port']
- #        else:
- #            out_port = ofproto.OFPP_FLOOD
- # 
- #        actions = [datapath.ofproto_parser.OFPActionOutput(out_port)]
- #        # install a flow to avoid packet_in next time
- #        if out_port != ofproto.OFPP_FLOOD:
- #            self.add_flow(datapath, in_port, dst, actions)
- # 
- #        out = datapath.ofproto_parser.OFPPacketOut(
- #           datapath=datapath, buffer_id=msg.buffer_id, in_port=in_port,
- #                    actions=actions)
- #        datapath.send_msg(out)
-   
-    @set_ev_cls(event.EventSwitchEnter)
-    def get_topology_data(self, ev):
-        switch_list = get_switch(self.topology_api_app, None)  
-        switches = [switch.dp.id for switch in switch_list]
-        self.net.add_nodes_from(switches)
-        
-        # print "**********List of switches"
-        for switch in switch_list:
-            self.ls(switch)
-            # print switch
-            self.nodes[self.no_of_nodes] = switch
-            self.no_of_nodes += 1
-       
-        links_list = get_link(self.topology_api_app, None)
-        # print links_list
-        links = [(link.src.dpid, link.dst.dpid, {'port': link.src.port_no}) for link in links_list]
-        # print links
-        self.net.add_edges_from(links)
-        links = [(link.dst.dpid, link.src.dpid, {'port': link.dst.port_no}) for link in links_list]
-        # print links
-        self.net.add_edges_from(links)
-        # print "**********List of links"
-        # print self.net.edges()
+        self.mac_to_port[dpid][src] = in_port
+        if dst in self.mac_to_port[dpid]:
+            out_port = self.mac_to_port[dpid][dst]
+        else:
+            out_port = ofproto.OFPP_FLOOD
+
+        actions = [parser.OFPActionOutput(out_port)]
