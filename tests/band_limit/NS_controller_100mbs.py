5ded7948d60f2bf706cef739edfdaa63cb764e4b
ErickSantosRodrigues <esrerickssantos@gmail.com>
2023-08-04 14:09:42 -0300

    att

--- tests/band_limit/NS_controller_100mbs.py
+++ tests/band_limit/NS_controller_100mbs.py
@@ -21,7 +21,7 @@ class NS_controller_100mbs(app_manager.RyuApp):
         meter_mod = parser.OFPMeterMod(datapath=datapath,
                                         command=ofproto.OFPMC_ADD,
                                         flags=ofproto.OFPMF_KBPS, meter_id=1,
-                                        bands=[parser.OFPMeterBandDrop(rate=50_000,
+                                        bands=[parser.OFPMeterBandDrop(rate=100_000,
                                                                         burst_size=0)])
         datapath.send_msg(meter_mod)
         # add a second meter entry with a rate limit of 100 Mbs
@@ -36,12 +36,12 @@ class NS_controller_100mbs(app_manager.RyuApp):
                                           ofproto.OFPCML_NO_BUFFER)]
         self.add_flow(datapath, 0, match, actions)
         # allow all communication from port 1
-        match = parser.OFPMatch(in_port=1)
+        match = parser.OFPMatch(eth_dst='00:00:00:00:00:01')
         actions = [parser.OFPActionOutput(ofproto.OFPP_IN_PORT), parser.OFPActionOutput(ofproto.OFPP_CONTROLLER)]
         self.add_flow(datapath, 1, match, actions, meter_id=1)
         # disiable all communication of mac address 00:00:00:00:00:02
         match = parser.OFPMatch(eth_dst='00:00:00:00:00:02')
-        actions = []
+        actions = [parser.OFPActionOutput(ofproto.OFPP_IN_PORT), parser.OFPActionOutput(ofproto.OFPP_CONTROLLER)]
         self.add_flow(datapath, 2, match, actions, meter_id=2)
         #
         # match = parser.OFPMatch(ipv4_dst='10.0.0.1')
