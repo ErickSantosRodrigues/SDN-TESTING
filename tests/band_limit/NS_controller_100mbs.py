9e35376c8714e264a076b405f8b306f1468238c9
ErickSantosRodrigues <esrerickssantos@gmail.com>
2023-08-04 14:18:13 -0300

    atta

--- tests/band_limit/NS_controller_100mbs.py
+++ tests/band_limit/NS_controller_100mbs.py
@@ -40,11 +40,11 @@ class NS_controller_100mbs(app_manager.RyuApp):
         actions = [parser.OFPActionOutput(ofproto.OFPP_IN_PORT), parser.OFPActionOutput(ofproto.OFPP_CONTROLLER)]
         self.add_flow(datapath, 1, match, actions)
         # allow all communication from port 1
-        match = parser.OFPMatch(eth_dst='00:00:00:00:00:02')
+        match = parser.OFPMatch(eth_dst='00:00:00:00:00:01', eth_src='00:00:00:00:00:02')
         actions = [parser.OFPActionOutput(ofproto.OFPP_IN_PORT), parser.OFPActionOutput(ofproto.OFPP_CONTROLLER)]
         self.add_flow(datapath, 2, match, actions, meter_id=1)
         # disiable all communication of mac address 00:00:00:00:00:02
-        match = parser.OFPMatch(eth_dst='00:00:00:00:00:03')
+        match = parser.OFPMatch(eth_dst='00:00:00:00:00:01', eth_src='00:00:00:00:00:03')
         actions = [parser.OFPActionOutput(ofproto.OFPP_IN_PORT), parser.OFPActionOutput(ofproto.OFPP_CONTROLLER)]
         self.add_flow(datapath, 2, match, actions, meter_id=2)
         #
