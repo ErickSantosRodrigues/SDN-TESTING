d3fb37800bd5f9ce935cb878684858b49c8a5057
ErickSantosRodrigues <esrerickssantos@gmail.com>
2023-08-11 15:55:55 -0300

    atualização

--- tests/Video_traffic/normal_traffic.py
+++ tests/Video_traffic/normal_traffic.py
@@ -34,7 +34,7 @@ class Controller_drop_h2(app_manager.RyuApp):
         # allow all communication from port 1
         match = parser.OFPMatch(in_port=1)
         actions = [parser.OFPActionOutput(ofproto.OFPP_IN_PORT), parser.OFPActionOutput(ofproto.OFPP_CONTROLLER)]
-        self.add_flow(datapath, 1, match, actions)
+        self.add_flow(datapath, 1, match, actions, meter_id=1)
         # # allow all communication from port 1
         # match = parser.OFPMatch(eth_dst='00:00:00:00:00:01', eth_src='00:00:00:00:00:02')
         # actions = [parser.OFPActionOutput(ofproto.OFPP_IN_PORT)]
