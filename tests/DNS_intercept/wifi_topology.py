15b2a1111428c730dbb4c9310b4d9005bc65f487
ErickSantosRodrigues <esrerickssantos@gmail.com>
2023-08-15 13:31:40 -0300

    testing if there is some diference between the communication

---
+++ tests/DNS_intercept/wifi_topology.py
@@ -0,0 +1,44 @@
+from mininet.node import RemoteController, OVSSwitch, CPULimitedHost
+from mininet.log import setLogLevel, info
+from mn_wifi.node import OVSKernelAP
+from mn_wifi.cli import CLI
+from mn_wifi.net import Mininet_wifi
+from mn_wifi.topo import Topo
+from time import sleep
+
+
+class Wifi_band_limit_Topo(Topo):
+    def __init__(self, **opts):
+        Topo.__init__(self, **opts)
+        info("*** Creating nodes\n")
+        sta1 = self.addStation('sta1', mac='00:00:00:00:00:01')
+        sta2 = self.addStation('sta2', mac='00:00:00:00:00:02')
+        sta3 = self.addStation('sta3', mac='00:00:00:00:00:03')
+
+        ap_arg = {'client_isolation': True}
+        ap1 = self.addAccessPoint('ap1', ssid='new-ssid', mode='ac', channel='36', position='50,50,0', **ap_arg)
+    
+        info("*** Associating Stations\n")
+        self.addLink(sta1, ap1, bw=150)
+        self.addLink(sta2, ap1, bw=150)
+        self.addLink(sta3, ap1, bw=150)
+
+
+def run():
+    net = Mininet_wifi(topo=Wifi_band_limit_Topo(), host=CPULimitedHost, controller=RemoteController('c', '127.0.0.1', 6653, protocols="OpenFlow13"), accessPoint=OVSKernelAP)
+    net.start()
+    sta1, sta2, sta3 = net.stations[0], net.stations[1], net.stations[2]
+    net.pingAll()
+    sta1.cmd('''xterm -geometry 80x24+0+0 -hold -T "sta1_5002" -e "iperf3 -s --port 5002 > server1.txt" &''')
+    sta1.cmd('''xterm -geometry 80x24+625+0 -hold -T "sta1_5003" -e "iperf3 -s --port 5003 > server2.txt" &''')
+    sleep(20)
+    sta2.cmd('''xterm -geometry 80x24+0+380 -hold -T "sta2" -e "iperf3 -c 10.0.0.1 --port 5002 -t 60 -i 60"&''')
+    sleep(10)
+    sta3.cmd('''xterm -geometry 80x24+625+380 -hold -T "sta3" -e "iperf3 -c 10.0.0.1 --port 5003 -t 20 -i 20" &''')
+    CLI(net)
+    net.stop()
+
+
+if __name__ == '__main__':
+    setLogLevel('info')
+    run()
--- tests/Video_traffic/normal_traffic.py
+++ tests/Video_traffic/normal_traffic.py
@@ -42,7 +42,7 @@ class Controller_drop_h2(app_manager.RyuApp):
         # disiable all communication of mac address 00:00:00:00:00:02
         match = parser.OFPMatch(eth_dst='00:00:00:00:00:03', eth_src='00:00:00:00:00:04')
         actions = [parser.OFPActionOutput(ofproto.OFPP_IN_PORT), parser.OFPActionOutput(ofproto.OFPP_CONTROLLER)]
-        self.add_flow(datapath, 2, match, actions, meter_id=2)
+        self.add_flow(datapath, 2, match, actions, meter_id=1)
 
     def add_flow(self, datapath, priority, match, actions, buffer_id=None, meter_id=None, command=None, idle_timeout=0):
         ofproto = datapath.ofproto
