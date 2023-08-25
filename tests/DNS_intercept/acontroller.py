1e8b3bd6403957ed5305cf0282985c999d90e288
ErickSantosRodrigues <esrerickssantos@gmail.com>
2023-08-25 15:31:08 -0300

    D4C

--- tests/DNS_intercept/acontroller.py
+++ tests/DNS_intercept/acontroller.py
@@ -2,7 +2,8 @@ from ryu.base import app_manager
 from ryu.controller import ofp_event
 from ryu.controller.handler import MAIN_DISPATCHER, CONFIG_DISPATCHER, set_ev_cls
 from ryu.ofproto import ofproto_v1_3
-from ryu.lib.packet import packet, ethernet, ether_types, ipv4, udp, dns
+from ryu.lib.packet import packet, ethernet, ether_types, ipv4, udp
+import dpkt
 
 
 class DNSApp(app_manager.RyuApp):
@@ -40,7 +41,6 @@ class DNSApp(app_manager.RyuApp):
         actions = [parser.OFPActionOutput(ofproto.OFPP_IN_PORT), parser.OFPActionOutput(ofproto.OFPP_CONTROLLER)]
         self.add_flow(datapath, 1, match, actions)
 
-
     @set_ev_cls(ofp_event.EventOFPPacketIn, MAIN_DISPATCHER)
     def _packet_in_handler(self, ev):
         msg = ev.msg
@@ -55,7 +55,7 @@ class DNSApp(app_manager.RyuApp):
             if ipv4_pkt.proto == 17:  # Check if the protocol is UDP 
                 udp_pkt = pkt.get_protocol(udp.udp)
                 if udp_pkt.dst_port == 53:  # Check if the destination port is 53 (DNS)
-                    dns_pkt = pkt.get_protocol(dns.dns)
-                    if dns_pkt is not None and dns_pkt.qr == 0:  # Check if it is a DNS query
-                        self.logger.info("DNS Query: %s", dns_pkt)
+                    dns_data = dpkt.dns.DNS(udp_pkt.data)
+                    if dns_data.qr == dpkt.dns.DNS_Q:   # Check if it is a DNS query 
+                        self.logger.info("DNS Query: %s", dns_data)
 
