a473ba8ededd5f04ab9c2553762d0697da16bbe4
ErickSantosRodrigues <esrerickssantos@gmail.com>
2023-08-30 14:45:08 -0300

    aaotaosas

--- tests/Video_traffic/blocked.py
+++ tests/Video_traffic/blocked.py
@@ -75,13 +75,14 @@ class Controller_drop_h2(app_manager.RyuApp):
         datapath = msg.datapath
         pkt = packet.Packet(msg.data)
         eth = pkt.get_protocol(ethernet.ethernet)
-        self.logger.info(f"Packet in: {eth.src} -> {eth.dst}")
-        if eth.dst == '00:00:00:00:00:03':
+        in_port = msg.match['in_port']
+        self.logger.info(f"Packet in {eth.src} ->{eth.dst} {in_port}")
+        if eth.src == '00:00:00:00:00:01' and eth.dst == '00:00:00:00:00:03':
             parser = datapath.ofproto_parser
-            match = parser.OFPMatch(eth_dst='00:00:00:00:00:02')
+            match = parser.OFPMatch(eth_dst='00:00:00:00:00:01', eth_src='00:00:00:00:00:02')
             # Drop the packets from h2
             actions = []
-            self.add_flow(datapath, 2, match, actions, idle_timeout=1)
+            self.add_flow(datapath, 3, match, actions, idle_timeout=1)
 
     @handler.set_ev_cls(ofp_event.EventOFPFlowRemoved, MAIN_DISPATCHER)
     def flow_removed_handler(self, ev):
