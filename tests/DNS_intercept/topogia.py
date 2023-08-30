0c8c0a11e7f8d7b779c4dac9bc1a7d4bda8de085
ErickSantosRodrigues <esrerickssantos@gmail.com>
2023-08-30 14:14:04 -0300

    aadas

---
+++ tests/DNS_intercept/topogia.py
@@ -0,0 +1,31 @@
+from mininet.node import Controller
+from mininet.log import setLogLevel, info
+from mn_wifi.cli import CLI_wifi
+from mn_wifi.net import Mininet_wifi
+
+
+def topology():
+    "Create a network."
+    net = Mininet_wifi(controller=Controller)
+    info("*** Creating nodes\n")
+    ap1 = net.addAccessPoint('ap1', ssid='new-ssid', mode='g', channel='1', position='10,10,0')
+    sta1 = net.addStation('sta1', position='10,20,0')
+    sta2 = net.addStation('sta2', position='10,30,0')
+    c1 = net.addController('c1', controller=Controller)
+    info("*** Configuring wifi nodes\n")
+    net.configureWifiNodes()
+    info("*** Starting network\n")
+    net.build()
+    net.addNAT(name='nat0', linkTo='ap1', ip='192.168.100.254').configDefault()
+    c1.start()
+    ap1.start([c1])
+    info("*** Running CLI\n")
+    CLI_wifi(net)
+    info("*** Stopping network\n")
+    net.stop()
+
+
+if __name__ == '__main__':
+    setLogLevel('info')
+    topology()
+
