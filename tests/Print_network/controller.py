ed56b620f0b48a223741696c9d1229ef24e621e0
ErickSantosRodrigues <esrerickssantos@gmail.com>
2023-09-01 07:25:47 -0300

    nx

---
+++ tests/Print_network/controller.py
@@ -0,0 +1,55 @@
+from ryu.base import app_manager
+from ryu.controller import ofp_event
+from ryu.controller.handler import MAIN_DISPATCHER, CONFIG_DISPATCHER
+from ryu.controller.handler import set_ev_cls
+from ryu.ofproto import ofproto_v1_3
+import networkx as nx
+
+class NetworkGraph(app_manager.RyuApp):
+    OFP_VERSIONS = [ofproto_v1_3.OFP_VERSION]
+    
+    def __init__(self, *args, **kwargs):
+        super(NetworkGraph, self).__init__(*args, **kwargs)
+        self.network_graph = nx.Graph()
+
+    @set_ev_cls(ofp_event.EventOFPSwitchFeatures, CONFIG_DISPATCHER)
+    def switch_features_handler(self, ev):
+        datapath = ev.msg.datapath
+        ofproto = datapath.ofproto
+        parser = datapath.ofproto_parser
+        # add a meter entry with a rate limit of 100 Mbs
+        meter_mod = parser.OFPMeterMod(datapath=datapath,
+                                        command=ofproto.OFPMC_ADD,
+                                        flags=ofproto.OFPMF_KBPS, meter_id=1,
+                                        bands=[parser.OFPMeterBandDrop(rate=100_000,
+                                                                        burst_size=0)])
+        datapath.send_msg(meter_mod)
+        # add a second meter entry with a rate limit of 100 Mbs
+        match = parser.OFPMatch()
+        actions = [parser.OFPActionOutput(ofproto.OFPP_CONTROLLER,
+                                          ofproto.OFPCML_NO_BUFFER)]
+        self.add_flow(datapath, 0, match, actions)
+        # allow all communication from port 1
+        match = parser.OFPMatch(in_port=1)
+        actions = [parser.OFPActionOutput(ofproto.OFPP_IN_PORT), parser.OFPActionOutput(ofproto.OFPP_CONTROLLER)]
+        self.add_flow(datapath, 1, match, actions)
+
+    @set_ev_cls(ofp_event.EventOFPPacketIn, MAIN_DISPATCHER)
+    def packet_in_handler(self, ev):
+        msg = ev.msg
+        dp = msg.datapath
+        ofp = dp.ofproto
+
+        src = msg.match['eth_src']
+        dst = msg.match['eth_dst']
+
+        # Update the network graph
+        self.network_graph.add_edge(src, dst)
+
+        # Print the network graph
+        self.print_network_graph()
+
+    def print_network_graph(self):
+        for edge in self.network_graph.edges():
+            self.logger.info("Edge: %s %s", edge[0], edge[1])
+
---
+++ tests/Print_network/wifi_topology.py
@@ -0,0 +1,44 @@
+from mininet.node import RemoteController, OVSSwitch, CPULimitedHost
+from mininet.log import setLogLevel, info
+from mn_wifi.node import OVSKernelAP
+from mn_wifi.cli import CLI
+from mn_wifi.net import Mininet_wifi
+from mn_wifi.topo import Topo
+from time import sleep
+
+
+class Wifi_band_limit_Topo(Topo):
+    def __init__(self, **opts):
+        Topo.__init__(self, **opts)
+        info("*** Creating nodes\n")
+        sta1 = self.addStation('sta1', mac='00:00:00:00:00:01')
+        sta2 = self.addStation('sta2', mac='00:00:00:00:00:02')
+        sta3 = self.addStation('sta3', mac='00:00:00:00:00:03')
+
+        ap_arg = {'client_isolation': True}
+        ap1 = self.addAccessPoint('ap1', ssid='new-ssid', mode='ac', channel='36', position='50,50,0', **ap_arg)
+    
+        info("*** Associating Stations\n")
+        self.addLink(sta1, ap1, bw=150)
+        self.addLink(sta2, ap1, bw=150)
+        self.addLink(sta3, ap1, bw=150)
+
+# Change the commands of each sta to solicitation of DNS
+def run():
+    net = Mininet_wifi(topo=Wifi_band_limit_Topo(), host=CPULimitedHost, controller=RemoteController('c', '127.0.0.1', 6653, protocols="OpenFlow13"), accessPoint=OVSKernelAP)
+    net.start()
+    net.addNAT(name='nat0', linkTo='ap1', ip='192.168.100.254').configDefault()
+    sta1, sta2, sta3 = net.stations[0], net.stations[1], net.stations[2]
+    net.pingAll()
+    sta1.cmd('''xterm -geometry 80x24+0+0 -hold -T "DNS_Solicitation_1" -e "dig +notcp www.example.com " &''')
+    sleep(20)
+    sta2.cmd('''xterm -geometry 80x24+625+0 -hold -T "DNS_Solicitation_2" -e "dig +notcp www.example.com " &''')
+    # sleep(10)
+    # sta3.cmd('''xterm -geometry 80x24+625+380 -hold -T "sta3" -e " " &''')
+    CLI(net)
+    net.stop()
+
+
+if __name__ == '__main__':
+    setLogLevel('info')
+    run()
